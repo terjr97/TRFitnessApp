6535e38fcdb413ad1687d3cfade368b415ca05fd
terjr97 <47218086+terjr97@users.noreply.github.com>
2019-10-23 16:09:41 -0400

    adding back the cmv

--- Routes/profile.js
+++ Routes/profile.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/:user/:password', (req, res) => {
-    console.log("router got");
-    res.send('hi');
-})
+router.get('/:user', async (req, res) => {
+    try{
+        const headers = {
+
+        }
+    
+        const {user} = req.params;
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message:'server error'
+        })
+    }
+});
 
 module.exports = router;
