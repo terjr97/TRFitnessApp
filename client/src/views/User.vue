7a6411c903dd0e84bc2d73e18401fb36aaf57a92
terjr97 <47218086+terjr97@users.noreply.github.com>
2019-11-29 23:39:09 -0500

    options Should be done

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -15,5 +15,16 @@
     <input class="input is-danger" type="text" placeholder="Enter Weight in Pounds">
   </div>
 </div>
+<button class="button is-info is-medium is-fullwidth">Set User Data</button>
+ <div class="panel">
+                <p class="panel-heading">
+                    Daily Calories
+                </p>
+            </div>
+ <div class="panel">
+                <p class="panel-heading">
+                    Remaining Calories
+                </p>
+            </div>
     </div>
 </template>
